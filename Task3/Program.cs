ec4da807c2e84ce97de18d5cb7c934a607739db5
AntonBelov <asbelov1907@gmail.com>
2022-05-16 23:16:54 +0300

    Задача 3 выполнена

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber(string msg)
+{
+while(true)
+{
+Console.WriteLine(msg);
+string valueFromConsole = Console.ReadLine();
+
+    if(int.TryParse(valueFromConsole, out int number))
+    {
+        if(number >= 1 )  
+        
+            return number;
+        else
+        {
+            Console.WriteLine("Число должно быть равным или больше 1. Введите другое число");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели не число. Нужно ввести число.");
+    }
+}
+}
+
+void GetCobe(int n)
+{
+    int index = 1;
+
+    while(index <= n)
+    {
+        Console.Write(Math.Pow(index,3) + " ");
+        index++;
+    }
+}
+
+int Number_N = GetNumber("Введите число равное или больше 1");
+GetCobe(Number_N);
